0b12cc9633f2d38aea4a8c3a7023e09794de2698
Karolina Viragova <karolinavi94@gmail.com>
2025-05-17 14:29:13 +0200

    changes to JourneyDetail

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,9 @@
 import { JourneyPicker } from '../../components/JourneyPicker';
+import { JourneyDetail } from '../../components/JourneyDetail/JourneyDetail';
 import { useState } from 'react';
 
 /*
-Upravte komponentu HomePage tak, aby v případě, kdy ve stavu journey je nějaké spojení, vypsala pod vyhledávací formulář text „Nalezeno spojení s id …“. Místo tří teček bude journeyId z dat o nalezeném spojení.
+Použijte komponentu JourneyDetail v komponentě HomePage na místě, kde se nyní vypisuje id nalezeného spoje. Komponenta se bude zobrazovat jenom tehdy, když ve stavu journey v komponentě HomePage je něco jiného než null. Ověřte, že se po vyhledání spojení na stránce zobrazí podrobnosti cesty s městy 1 až 4 (komponenta JourneyDetail zatím není napojená na reálná data).
 */
 
 export const HomePage = () => {
@@ -17,7 +18,8 @@ export const HomePage = () => {
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange}/>
-      {journey ? <p>Nalezeno spojení s id {journey.journeyId}</p> : ""}
+      {journey && <JourneyDetail journey={journey}/>}
     </main>
   );
 };
+
